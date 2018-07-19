eb21706b120919b9075bd297619dc1ee8fa88e8c
Nash Taylor <ntaylorwss@outlook.com>
2018-07-19 13:45:51 -0700

    Surpress skimage warning

--- pavlov/pipeline/__init__.py
+++ pavlov/pipeline/__init__.py
@@ -1,3 +1,7 @@
+import warnings
+warnings.filterwarnings('ignore', module='skimage')
+from skimage.transform import resize
+
 from . import pipeline
 from . import transformations
 from .pipeline import *
