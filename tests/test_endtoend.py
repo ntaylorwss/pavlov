0e3847691b04d200a347bc6b83c5210b61ff0d4e
Nash Taylor <ntaylorwss@outlook.com>
2018-07-19 13:46:04 -0700

    Test all kinds of environments and models

--- tests/test_endtoend.py
+++ tests/test_endtoend.py
@@ -1,17 +1,18 @@
 import sys
 sys.path.append('/home')
 
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 import pavlov
 import gym
-import keras.optimizers
+import tensorflow as tf
+
 
 class TestPavlov:
-    def setup(self):
-        self.basic_discrete_env = gym.make("CartPole-v0")
-        self.intermediate_discrete_env = gym.make("Breakout-v0")
-
     def test_basic_discrete_agent(self):
         # arrange
+        env = gym.make("CartPole-v0")
         base_config = {
             'layer_sizes': [128],
             'activation': 'relu'
@@ -19,7 +20,7 @@ class TestPavlov:
         dqn_config = {
             'gamma': 0.99,
             'tau': 1.0,
-            'optimizer': keras.optimizers.SGD(lr=0.03)
+            'optimizer': tf.keras.optimizers.SGD(lr=0.03)
         }
         topology = pavlov.models.topology.DenseTopology(**base_config)
         model = pavlov.models.DQNModel(topology, **dqn_config)
@@ -27,20 +28,22 @@ class TestPavlov:
         epsilon_schedule = pavlov.auxiliary.schedules.LinearDecaySchedule(1.0, 0.1, 500, -1)
         actor = pavlov.actors.EpsilonGreedyActor(epsilon_schedule)
         buffer_size = 10000
-        batch_size = 64
+        batch_size = 2
 
         pline = pavlov.pipeline.Pipeline()
-        agent = pavlov.agents.Agent(self.basic_discrete_env,
+        agent = pavlov.agents.Agent(env,
                                     state_pipeline=pline,
                                     model=model, actor=actor,
                                     buffer_size=buffer_size, batch_size=batch_size,
                                     report_frequency=10, warmup_length=50)
 
+
         # act
         agent.run_episode(render=False, do_logging=True)
+        print("Done")
 
-"""
     def test_intermediate_discrete_env(self):
+        env = gym.make('Breakout-v0')
         topology_config = {
             'layer_sizes': [128],
             'activation': 'relu'
@@ -50,7 +53,7 @@ class TestPavlov:
         dqn_config = {
             'gamma': 0.99,
             'tau': 1.0,
-            'optimizer': keras.optimizers.Adam(0.0001)
+            'optimizer': tf.keras.optimizers.Adam(0.0001)
         }
         model = pavlov.models.DQNModel(topology, **dqn_config)
 
@@ -65,10 +68,40 @@ class TestPavlov:
         pline.add(pavlov.transformations.combine_consecutive(2, 'max'))
         pline.add(pavlov.transformations.stack_consecutive(4))
 
-        agent = pavlov.agents.Agent(self.intermediate_discrete_env,
+        agent = pavlov.agents.Agent(env,
                                     state_pipeline=pline,
                                     model=model, actor=actor,
                                     buffer_size=buffer_size, batch_size=batch_size,
                                     report_frequency=1, warmup_length=5)
         agent.run_episode(render=False, do_logging=True)
-"""
+        print("Done")
+
+    def test_basic_continuous_env(self):
+        env = gym.make('MountainCarContinuous-v0')
+        base_config = {
+            'layer_sizes': [128],
+            'activation': 'relu'
+        }
+        ddpg_config = {
+            'actor_activation': 'softmax',
+            'gamma': 0.99,
+            'tau': 0.1,
+            'actor_optimizer': tf.train.AdamOptimizer(0.0001),
+            'critic_optimizer': tf.keras.optimizers.Adam(lr=0.0001)
+        }
+        topology = pavlov.models.topology.DenseTopology(**base_config)
+        model = pavlov.models.DDPGModel(topology, **ddpg_config)
+
+        epsilon_schedule = pavlov.auxiliary.schedules.LinearDecaySchedule(1.0, 0.1, 500, -1)
+        actor = pavlov.actors.EpsilonGreedyActor(epsilon_schedule)
+        buffer_size = 10000
+        batch_size = 64
+
+        pline = pavlov.pipeline.Pipeline()
+        agent = pavlov.agents.Agent(env,
+                                    state_pipeline=pline,
+                                    model=model, actor=actor,
+                                    buffer_size=buffer_size, batch_size=batch_size,
+                                    report_frequency=1, warmup_length=50)
+        agent.run_episode(render=False, do_logging=True)
+        print("Done")
